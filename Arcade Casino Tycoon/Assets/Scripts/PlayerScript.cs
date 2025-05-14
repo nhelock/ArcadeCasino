f97dc5498961ff8ef3b5280a0d8edd1a7d39496e
gagekta <gc.99hockey@gmail.com>
2025-05-13 23:39:19 -0400

    one last fix for the road ggez no re

--- Arcade Casino Tycoon/Assets/Scripts/PlayerScript.cs
+++ Arcade Casino Tycoon/Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +5,16 @@ public class PlayerScript : MonoBehaviour
 {
     // --- This script is for BOTH player and dealer
 
-    public CardScript cardScript;   
+    public CardScript cardScript;
     public DeckScript deckScript;
 
     public int handValue = 0;
 
-    private int money = 1000;
-
     public GameObject[] hand;
     public int cardIndex = 0;
+
     // Tracking aces for 1 to 11 conversions
-    List<CardScript> aceList = new List<CardScript>();
+    private List<CardScript> aceList = new List<CardScript>();
 
     public void StartHand()
     {
@@ -26,32 +24,33 @@ public class PlayerScript : MonoBehaviour
 
     public int GetCard()
     {
-        // Get a card, use deal card to assign sprite and value to card on table
         int cardValue = deckScript.DealCard(hand[cardIndex].GetComponent<CardScript>());
-        // Show card on game screen
+
         hand[cardIndex].GetComponent<Renderer>().enabled = true;
-        // Add card value to running total of the hand
+
         handValue += cardValue;
-        // If value is 1, it is an ace
-        if(cardValue == 1)
+
+        if (cardValue == 1)
         {
             aceList.Add(hand[cardIndex].GetComponent<CardScript>());
         }
+
         AceCheck();
         cardIndex++;
+
         return handValue;
     }
 
-    // Search for needed ace conversions, 1 to 11 or vice versa
     public void AceCheck()
     {
         foreach (CardScript ace in aceList)
         {
-            if(handValue + 10 < 22 && ace.GetValueOfCard() == 1)
+            if (handValue + 10 < 22 && ace.GetValueOfCard() == 1)
             {
                 ace.SetValue(11);
                 handValue += 10;
-            } else if (handValue > 21 && ace.GetValueOfCard() == 11)
+            }
+            else if (handValue > 21 && ace.GetValueOfCard() == 11)
             {
                 ace.SetValue(1);
                 handValue -= 10;
@@ -61,21 +60,34 @@ public class PlayerScript : MonoBehaviour
 
     public void AdjustMoney(int amount)
     {
-        money += amount;
+        if (amount >= 0)
+        {
+            WalletManager.Instance.AddMoney(amount);
+        }
+        else
+        {
+            // Try to subtract only if enough money
+            bool success = WalletManager.Instance.SubtractMoney(Mathf.Abs(amount));
+            if (!success)
+            {
+                Debug.LogWarning("Not enough funds to subtract " + Mathf.Abs(amount));
+            }
+        }
     }
 
     public int GetMoney()
     {
-        return money;
+        return WalletManager.Instance.Wallet;
     }
 
     public void ResetHand()
     {
-        for(int i = 0; i < hand.Length; i++)
+        for (int i = 0; i < hand.Length; i++)
         {
             hand[i].GetComponent<CardScript>().ResetCard();
             hand[i].GetComponent<Renderer>().enabled = false;
         }
+
         cardIndex = 0;
         handValue = 0;
         aceList = new List<CardScript>();
