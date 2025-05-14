b76ee7b2829c16f004b87f1e1523f57dea4e7cd3
gagekta <gc.99hockey@gmail.com>
2025-05-13 21:44:11 -0400

    back button functionality
    FINALLY

--- Arcade Casino Tycoon/Assets/Scripts/MainMenu.cs
+++ Arcade Casino Tycoon/Assets/Scripts/MainMenu.cs
@@ -1,43 +1,82 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public static MainMenu Instance { get; private set; }
+
     public GameObject MenuPanel;
     public GameObject GameMenu;
+
 
     private void Start()
     {
         ShowMainMenu();
     }
 
-    public void ShowMainMenu()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        MenuPanel.SetActive(true);
-        if (OptionsMenu.Instance != null)
-            OptionsMenu.Instance.gameObject.SetActive(false);
-
-        GameMenu.SetActive(false);
+        RefreshUIReferences();
+        ShowMainMenu();
     }
 
-    public void ShowOptions()
+    private void RefreshUIReferences()
+{
+    // Ensure the GameObjects are correctly found
+    if (MenuPanel == null)
     {
-        MenuPanel.SetActive(false);
-        if (OptionsMenu.Instance != null)
-            OptionsMenu.Instance.gameObject.SetActive(true);
+        MenuPanel = GameObject.Find("MenuPanel");
+        if (MenuPanel == null) Debug.LogError("MenuPanel not found!");
+    }
 
-        GameMenu.SetActive(false);
+    if (GameMenu == null)
+    {
+        GameMenu = GameObject.Find("GameMenu");
+        if (GameMenu == null) Debug.LogError("GameMenu not found!");
+    }
+}
+
+    public void ShowMainMenu()
+    {
+        RefreshUIReferences();
+
+        if (MenuPanel != null) MenuPanel.SetActive(true);
+        if (GameMenu != null) GameMenu.SetActive(false);
     }
 
     public void ShowGameMenu()
+{
+    Debug.Log("Showing Game Menu...");
+
+    // Refresh UI references
+    RefreshUIReferences();
+
+    // Deactivate the MenuPanel and activate the GameMenu
+    if (MenuPanel != null)
     {
         MenuPanel.SetActive(false);
-        if (OptionsMenu.Instance != null)
-            OptionsMenu.Instance.gameObject.SetActive(false);
-
-        GameMenu.SetActive(true);
+        Debug.Log("MenuPanel set to inactive.");
+    }
+    else
+    {
+        Debug.LogWarning("MenuPanel is null.");
     }
 
-    public void NewGame() => ShowGameMenu();
+    if (GameMenu != null)
+    {
+        GameMenu.SetActive(true);
+        Debug.Log("GameMenu set to active.");
+    }
+    else
+    {
+        Debug.LogWarning("GameMenu is null.");
+    }
+}
+
+    public void NewGame()
+    {
+        ShowGameMenu();
+    }
 
     public void LoadGame()
     {
@@ -46,7 +85,7 @@ public class MainMenu : MonoBehaviour
             SaveData data = SaveSystem.Load();
             if (data != null)
             {
-                MenuGameManager.Instance.ApplySaveData(data); 
+                MenuGameManager.Instance.ApplySaveData(data);
                 ShowGameMenu();
             }
             else
@@ -60,12 +99,14 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-
     public void ExitGame()
     {
         Debug.Log("Exiting Game...");
         Application.Quit();
     }
 
-    public void Back() => ShowMainMenu();
+    public void Back()
+    {
+        ShowMainMenu();
+    }
 }
